db63027d1f13dc15f948e7e1eb0739ea44c0226f
fabdan <fabiodaniele01@gmail.com>
2020-02-19 05:14:53 +0100

    First commit of "Google Maps Embed Fallback"

--- resources/views/Public/ViewEvent/Partials/EventMapSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventMapSection.blade.php
@@ -2,7 +2,11 @@
     <div class="row">
         <div class="col-md-12">
             <div class="google-maps content">
+                <?php if(!empty(config("attendize.google_maps_geocoding_key"))): ?>
                 <iframe frameborder="0" style="border:0;" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q={{$event->map_address}}&amp;aq=0&amp;oq={{$event->map_address}}&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq={{$event->map_address}}&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+                <?php elseif(!empty(config("attendize.google_maps_embed_key"))): ?>
+                <iframe frameborder="0" style="border:0;" src="https://www.google.com/maps/embed/v1/place?key={{config("attendize.google_maps_embed_key")}}&q={{$event->location_address_line_1}}%20{{$event->location_address_line_1}}%20{{$event->location_state}}%20{{$event->location_post_code}}"></iframe>
+                <?php endif; ?>
             </div>
         </div>
     </div>
