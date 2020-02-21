36b55ae04e6e7edb58d9f4a9b99f0ca1ce4f0dc7
fabdan <fabiodaniele01@gmail.com>
2020-02-21 12:36:38 +0100

    ~

--- resources/views/Public/ViewOrganiser/Partials/OrganiserFooterSection.blade.php
+++ resources/views/Public/ViewOrganiser/Partials/OrganiserFooterSection.blade.php
@@ -1,6 +1,6 @@
 <footer id="footer" class="container-fluid">
     <div class="col-md-8 col-md-offset-2">
-        <div class="col-md-3 navigation">
+        <div class="col-md-3 col-xs-6 navigation">
             <h3>Navigazione</h3>
             <a href="https://www.voltitalia.org/">Home</a>
             <a href="https://www.voltitalia.org/vision">Visione</a>
@@ -10,14 +10,14 @@
             <a href="https://www.voltitalia.org/donate">Dona</a>
         </div>
 
-        <div class="col-md-3 navigation">
+        <div class="col-md-3 col-xs-6 navigation">
             <h3>Altre pagine</h3>
             <a href="https://www.voltitalia.org/transparency">Trasparenza</a>
             <a href="https://www.voltitalia.org/elezioni_trasparenti">Elezioni trasparenti</a>
             <a href="https://www.voltitalia.org/statuto">Statuto</a>
         </div>
 
-        <div class="col-md-6 navigation">
+        <div class="col-md-6 col-xs-12 navigation">
             <h3>Contatti</h3>
             <p>Domande generiche e commenti: <a href="mailto:info@voltitalia.org">info@voltitalia.org</a></p>
             <p>Richieste da stampa e media: <a href="mailto:davide.la.cara@volteuropa.org">davide.la.cara@volteuropa.org</a></p>
@@ -25,7 +25,7 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-12" style="margin-top: 15px;margin-bottom: 10px;font-size: 11px;">
+            <div class="col-md-12 col-xs-12" style="margin-top: 15px;margin-bottom: 10px;font-size: 11px;">
                 {{--Attendize is provided free of charge on the condition the below hyperlink is left in place.--}}
                 {{--See https://github.com/Attendize/Attendize/blob/master/LICENSE for more information.--}}
                 @include('Shared.Partials.PoweredBy')
--- resources/views/Public/ViewOrganiser/Partials/OrganiserHeaderSection.blade.php
+++ resources/views/Public/ViewOrganiser/Partials/OrganiserHeaderSection.blade.php
@@ -1,11 +1,11 @@
 <section id="header">
     <div class="col-md-8 col-md-offset-2">
-        <div class="col-md-6">
+        <div class="col-md-6 col-xs-6">
             <a href="https://voltitalia.org" class="logo">
                 <img src="{{URL::to($organiser->full_logo_path)}}" width="70px" style="width: 70px;">
             </a>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 col-xs-6">
             <a href="https://voltitalia.org" class="home-menu">Home</a>
         </div>
     </div>
